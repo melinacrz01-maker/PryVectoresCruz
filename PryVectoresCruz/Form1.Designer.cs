1f6f06cbdb8efe092ab7a6f80b5e5d8fbe7d05d3
Alumno <Alumno@PC158>
2026-08-12 13:19:25 -0300

    Agregar archivos de proyecto.

---
+++ PryVectoresCruz/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace PryVectoresCruz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMes = new System.Windows.Forms.Label();
+            this.lblImporte = new System.Windows.Forms.Label();
+            this.txtImporte = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmbmes = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnCargaFor = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.DgvImporte = new System.Windows.Forms.DataGridView();
+            this.Mes = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvImporte)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblMes
+            // 
+            this.lblMes.AutoSize = true;
+            this.lblMes.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMes.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblMes.Location = new System.Drawing.Point(31, 61);
+            this.lblMes.Name = "lblMes";
+            this.lblMes.Size = new System.Drawing.Size(45, 18);
+            this.lblMes.TabIndex = 1;
+            this.lblMes.Text = "Mes:";
+            // 
+            // lblImporte
+            // 
+            this.lblImporte.AutoSize = true;
+            this.lblImporte.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImporte.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblImporte.Location = new System.Drawing.Point(31, 103);
+            this.lblImporte.Name = "lblImporte";
+            this.lblImporte.Size = new System.Drawing.Size(71, 16);
+            this.lblImporte.TabIndex = 3;
+            this.lblImporte.Text = "Importes:";
+            // 
+            // txtImporte
+            // 
+            this.txtImporte.Location = new System.Drawing.Point(96, 86);
+            this.txtImporte.Name = "txtImporte";
+            this.txtImporte.Size = new System.Drawing.Size(102, 20);
+            this.txtImporte.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(206, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 31);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Cargar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmbmes
+            // 
+            this.cmbmes.FormattingEnabled = true;
+            this.cmbmes.Location = new System.Drawing.Point(82, 58);
+            this.cmbmes.Name = "cmbmes";
+            this.cmbmes.Size = new System.Drawing.Size(100, 21);
+            this.cmbmes.TabIndex = 6;
+            this.cmbmes.SelectedIndexChanged += new System.EventHandler(this.cmbmes_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtImporte);
+            this.groupBox1.Location = new System.Drawing.Point(12, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(318, 190);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Carga de Datos";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.DgvImporte);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.btnCargaFor);
+            this.groupBox2.Location = new System.Drawing.Point(12, 231);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(318, 228);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // btnCargaFor
+            // 
+            this.btnCargaFor.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCargaFor.Location = new System.Drawing.Point(115, 199);
+            this.btnCargaFor.Name = "btnCargaFor";
+            this.btnCargaFor.Size = new System.Drawing.Size(75, 23);
+            this.btnCargaFor.TabIndex = 1;
+            this.btnCargaFor.Text = "Carga For";
+            this.btnCargaFor.UseVisualStyleBackColor = true;
+            this.btnCargaFor.Click += new System.EventHandler(this.btnCargaFor_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(215, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Listar con While";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // DgvImporte
+            // 
+            this.DgvImporte.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvImporte.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Mes,
+            this.Column2});
+            this.DgvImporte.Location = new System.Drawing.Point(6, 30);
+            this.DgvImporte.Name = "DgvImporte";
+            this.DgvImporte.Size = new System.Drawing.Size(243, 150);
+            this.DgvImporte.TabIndex = 3;
+            // 
+            // Mes
+            // 
+            this.Mes.HeaderText = "Mes";
+            this.Mes.Name = "Mes";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Importe";
+            this.Column2.Name = "Column2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(342, 502);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.cmbmes);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblImporte);
+            this.Controls.Add(this.lblMes);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DgvImporte)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblMes;
+        private System.Windows.Forms.Label lblImporte;
+        private System.Windows.Forms.TextBox txtImporte;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmbmes;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnCargaFor;
+        private System.Windows.Forms.DataGridView DgvImporte;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Mes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PryVectoresCruz/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization.Configuration;
+
+namespace PryVectoresCruz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        String[] Meses = new string[12] { "Enero","Febrero","Marzo", "Abril", "Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
+        Decimal[] Importe = new Decimal[12];
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = cmbmes.SelectedIndex;
+            Decimal importe = Convert.ToDecimal(txtImporte);
+            Importe[i] = importe;
+            
+            MessageBox.Show("Dato cargado correctamente");
+            
+        }
+
+        private void cmbmes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Meses.Length; i++)
+            {
+                cmbmes.Items.Add(Meses[i]);
+            }
+            cmbmes.SelectedIndex = 0;
+        }
+
+        private void btnCargaFor_Click(object sender, EventArgs e)
+        {
+            DgvImporte.Rows.Clear();
+            for (int i = 0; i < 12; i++)
+            {
+                DgvImporte.Rows.Add(Meses[i], Importe[i]);
+            }
+        }
+        
+    }
+}
